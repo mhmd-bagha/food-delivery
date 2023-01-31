3c12bbc9119b1f1d21c7e5730b97719a0cc6cfa0
mhmddev <mabrahimibagha@gmail.com>
2023-01-31 16:50:57 +0330

    Redesign payment

--- client/src/components/pages/payment/footer.jsx
+++ client/src/components/pages/payment/footer.jsx
@@ -4,14 +4,15 @@ const Footer = () => {
     return (
         <>
             {/* total price and next top level */}
-            <div className="bg_mirage py-10 px-12 flex justify-between items-center lg:pt-36 xl:pt-40">
+            <div className="bg_mirage py-10 px-5 sm:px-12 flex justify-between items-center lg:pt-36 xl:pt-40">
                 {/* total price */}
                 <div>
                     <p className="text-sm text-gray-400">Total Price</p>
                     <p className="text-2xl font-bold text-gray-100">$33,50</p>
                 </div>
                 {/* next to top level */}
-                <Link to="/delivery-information" className="bg_red_coral text-white font-bold px-14 py-4 rounded-2xl">Order Now</Link>
+                <Link to="/delivery-information" className="bg_red_coral text-white font-bold px-12 sm:px-14 py-4 rounded-2xl">Order
+                    Now</Link>
             </div>
         </>
     )
