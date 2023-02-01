3b7d341885022961d6de90b2421ae94018f677a5
mhmddev <mabrahimibagha@gmail.com>
2023-02-01 17:04:59 +0330

    refactor: add total price and next and change class

--- client/src/components/pages/payment/index.jsx
+++ client/src/components/pages/payment/index.jsx
@@ -1,15 +1,15 @@
-import Footer from "./footer";
 import "./styles.scss";
 import "../../../styles/forms.scss";
 import PaymentList from "./payment-list";
 import Header from "./header";
 import CouponTaxation from "./coupon-taxation";
+import TotalPriceNext from "../../ui/total-price-next";
 
 const Payment = () => {
     return (
         <>
             {/* background full */}
-            <section className="bg_mirage min-h-full">
+            <section className="bg_mirage h-full">
                 {/* start page */}
                 <div className="bg_dark px-12 pt-8 h-5/6 rounded-b-main">
                     {/* header */}
@@ -20,7 +20,7 @@ const Payment = () => {
                     <CouponTaxation/>
                 </div>
                 {/* total price and next top level */}
-                <Footer/>
+                <TotalPriceNext totalPrice='33,50' linkText='Order Now' linkUrl='delivery-information'/>
             </section>
         </>
     )
---
+++ client/src/components/ui/total-price-next/index.jsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+
+const TotalPriceNext = ({linkText, linkUrl, totalPrice}) => {
+    return (
+        <>
+            {/* total price and next top level */}
+            <div className="bg_mirage py-10 px-5 sm:px-12 flex justify-between items-center">
+                {/* total price */}
+                <div>
+                    <p className="text-sm text-gray-400">Total Price</p>
+                    <p className="text-2xl font-bold text-gray-100">${totalPrice}</p>
+                </div>
+                {/* next to top level */}
+                <Link to={`/` + linkUrl}
+                      className="bg_red_coral text-white font-bold w-3/5 sm:w-1/3 xl:w-1/5 2xl:w-1/6 py-4 rounded-2xl text-center">{linkText}</Link>
+            </div>
+        </>
+    )
+}
+export default TotalPriceNext
