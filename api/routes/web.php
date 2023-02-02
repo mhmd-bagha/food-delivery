5a3f9254f87b2a1df67bc572705a220cf55d4e5d
mhmddev <mabrahimibagha@gmail.com>
2023-02-02 13:44:19 +0330

    perf: add api the sub routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -18,32 +18,34 @@ use \App\Http\Controllers\FavoriteController;
 |
 */
 
-// the routes api on food
-Route::prefix('food')->group(function () {
-    // get all foods from category
-    Route::post('/category/{category}', [FoodController::class, 'foodsCategory']);
-    // get all categories
-    Route::post('/categories', [FoodCategoryController::class, 'getCategories']);
-    // get data food
-    Route::post('/get/{foodId}', [FoodController::class, 'food']);
-});
+Route::prefix('api/')->group(function () {
+    // the routes api on food
+    Route::prefix('food')->group(function () {
+        // get all foods from category
+        Route::post('/category/{category}', [FoodController::class, 'foodsCategory']);
+        // get all categories
+        Route::post('/categories', [FoodCategoryController::class, 'getCategories']);
+        // get data food
+        Route::post('/get/{foodId}', [FoodController::class, 'food']);
+    });
 // the routes user
-Route::prefix('user')->group(function () {
-    // login and signup
-    Route::post('/login', [UserController::class, 'login']);
-    Route::post('/signup', [UserController::class, 'signup']);
-    // forgot password and change password
-    Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
-    Route::post('/change-password', [UserController::class, 'changePassword']);
-});
+    Route::prefix('user')->group(function () {
+        // login and signup
+        Route::post('/login', [UserController::class, 'login']);
+        Route::post('/signup', [UserController::class, 'signup']);
+        // forgot password and change password
+        Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
+        Route::post('/change-password', [UserController::class, 'changePassword']);
+    });
 // the routes cart
-Route::prefix('cart')->group(function () {
-    Route::post('/add', [CartController::class, 'addFood']);
-    Route::post('/get', [CartController::class, 'getCart']);
-    Route::delete('/delete/cart-id/{cart_id}', [CartController::class, 'deleteFood']);
-});
+    Route::prefix('cart')->group(function () {
+        Route::post('/add', [CartController::class, 'addFood']);
+        Route::post('/get', [CartController::class, 'getCart']);
+        Route::delete('/delete/cart-id/{cart_id}', [CartController::class, 'deleteFood']);
+    });
 // add/get favorite
-Route::prefix('favorites')->group(function () {
-    Route::post('/', [FavoriteController::class, 'getFavorite']);
-    Route::post('/add', [FavoriteController::class, 'addFavorite']);
+    Route::prefix('favorites')->group(function () {
+        Route::post('/', [FavoriteController::class, 'getFavorite']);
+        Route::post('/add', [FavoriteController::class, 'addFavorite']);
+    });
 });
