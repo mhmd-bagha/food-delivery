43bb608cb375fc008e1ee21da5bb98b770900943
mhmddev <mabrahimibagha@gmail.com>
2023-02-13 16:46:15 +0330

    perf(priceFormat): add replace value

--- client/src/components/tools/price-format.js
+++ client/src/components/tools/price-format.js
@@ -1,8 +1,8 @@
-const PriceFormat = (price) => {
+const PriceFormat = (price, characterReplace = ',') => {
     // regex price format
     const formatPrice = /\B(?=(\d{2})+(?!\d))/g
 
-    return price.toString().replace(formatPrice, ",")
+    return price.toString().replace(formatPrice, characterReplace)
 }
 
 export default PriceFormat
