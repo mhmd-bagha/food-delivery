c858457c609cbf16ee794fdf88a98dc30d718db6
mhmddev <mabrahimibagha@gmail.com>
2023-03-05 09:13:02 +0330

    perf: add client host to csrfToken

--- api/app/Http/Middleware/VerifyCsrfToken.php
+++ api/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '*'
+        'http://localhost:3000',
+        'http://sample-food-store.surge.sh'
     ];
 }
