eaa6beb380c05001b5c6d96420d3267f3c7aa15b
mhmddev <mabrahimibagha@gmail.com>
2023-03-08 09:25:16 +0330

    fix: fixed bug get token

--- client/src/components/tools/auth-user.js
+++ client/src/components/tools/auth-user.js
@@ -9,10 +9,16 @@ export const ExpiredToken = (token) => {
 }
 
 export const GetToken = () => {
-    if (localStorage.getItem('persist:root')) {
+    // if (localStorage.getItem('persist:root')) {
+    //     const user = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user) // get user from localStorage and json parse
+    //     return user.token
+    // } else return null
+    try {
         const user = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user) // get user from localStorage and json parse
         return user.token
-    } else return null
+    } catch (err) {
+        return null
+    }
 }
 
 export const useRefreshToken = (user, refreshToken) => {
