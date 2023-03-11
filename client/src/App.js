490b360299b44b64705544ab8e9b7cce12e1726f
mhmddev <mabrahimibagha@gmail.com>
2023-03-11 18:56:54 +0330

    perf: get user from useSelector

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import {Routes} from "react-router-dom";
 import './styles/index.css'
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 import Protected from "./routes/protected";
 import Public from "./routes/public";
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify";
 
-function App({user}) {
+function App() {
+    const user = useSelector(state => state.user)
+
     return (
         <>
             {/* import toast container */}
@@ -19,10 +21,4 @@ function App({user}) {
     );
 }
 
-const mapToStateProps = (state) => {
-    return {
-        user: state.user
-    }
-}
-
-export default connect(mapToStateProps, null)(App)
+export default App
